c9a7cc34e4cf08fe321a1319783f2eb67a271bf6
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-02-13 23:04:35 -0300

    Exemplo elaborado em aula

---
+++ Aula_01/Aula/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string nomePessoa = "Emily";
+            string dataNascimento = "19/02/1999";
+            string rg = "0.000.000";
+            char   sexo = 'F';
+            double salario = 10000.00;
+            string cidade;
+
+            Console.WriteLine("O Senhor(a) " + nomePessoa + ", portador(a) do RG de número " + rg + ", nascido em " + dataNascimento + ", do sexo " + sexo + ", está registrado com o salário de R$ " + salario + ".");
+            Console.ReadKey(); // irá aguardar clicar em qualquer tela para continuar o programa
+
+            Console.Write("Informe sua cidade: ");
+            cidade = Console.ReadLine();
+
+            Console.WriteLine("Cidade: " + cidade);
+
+            /*
+             * Para converter de String para Char, pode ser feito da seguinte forma:
+             * sexo = Char.Parse(Console.ReadLine());
+             * 
+             * Para converter de String para Double:
+             * salario = Double.Parse(Console.ReadLine());
+             */
+        }        
+    }
+}
