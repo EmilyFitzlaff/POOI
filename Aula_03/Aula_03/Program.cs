eebcf16d2155a0df47105c54b4df89572b475954
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-03-08 12:32:22 -0300

    Ajuste dos arquivos das aulas

---
+++ Aula_03/Aula_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exercício 01
+            String nome, rg, dataNascimento;
+            char sexo;
+            Boolean tipoContratacao;
+            double salario;
+
+            Console.WriteLine("Informe seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe seu RG: ");
+            rg = Console.ReadLine();
+
+            Console.WriteLine("Informe sua data de nascimento: ");
+            dataNascimento = Console.ReadLine();
+
+            Console.WriteLine("Informe 'F' para Feminino ou 'M' para masculino: ");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("Tipo de Contratação: ")
+
+
+        }
+    }
+}
---
+++ Aula_04/Aula_04/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Aula_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exercício 01
+            int[] arrayNumeros = new int[5];
+
+            arrayNumeros[0] = 0;
+            arrayNumeros[1] = 34;
+            arrayNumeros[2] = 21;
+            arrayNumeros[3] = 45;
+            arrayNumeros[4] = 15;
+
+            for (int i = 0; i <= arrayNumeros.Length - 1; i++)
+            {
+                Console.WriteLine("Índice: " + arrayNumeros[i]);
+            }
+
+            Console.WriteLine("-------------------------");
+
+            // exercício 02
+            String[] arrayEmails = {"1: Cliente 1 Email: cliente1@yahoo.com.br",
+                                    "2: Cliente 2 Email: cliente2@yahoo.com.br",
+                                    "3: Cliente 3 Email: cliente3@yahoo.com.br",
+                                    "4: Cliente 4 Email: cliente4@yahoo.com.br",
+                                    "5: Cliente 5 Email: cliente5@yahoo.com.br" };
+
+            for (int i = 0; i <= arrayEmails.Length - 1; i++)
+            {
+                Console.WriteLine(arrayEmails[i]);
+            }
+
+            Console.WriteLine("-------------------------");
+
+            // exercício 03
+
+            int[,] arrayBidimensional = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i = 0; i <= arrayBidimensional.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j <= arrayBidimensional.GetLength(1) - 1; j++)
+                {
+                    Console.WriteLine("Posição: " + i + " - " + j + ": " + arrayBidimensional[i, j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
